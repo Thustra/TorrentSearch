ea912c5d7138086a6a157da09532707de61ec135
Thustra <peter.windey@telenet.be>
2015-05-20 00:08:50 +0200

    added: Moving files to the NAS bugfixes: / Known bugs: Search fails for episodes lower than 10

--- cleardownloads.py
+++ cleardownloads.py
@@ -7,7 +7,8 @@ from os import remove,rename
 import scan,rename_file
 
 
-ROOT_DIR = 'E:\Downloads - finished\\'
+ROOT_SRC_DIR = 'E:\Downloads - finished\\'
+ROOT_TRG_DIR = 'Z:\Series\\'
 
 def flatten_directory(dir):
     root_path=dir
@@ -18,8 +19,8 @@ def flatten_directory(dir):
     filelist = scan.scan_all_files(dir)
     for file in filelist:
         print('Copying file from ' + dir+file + ' to ' + root_path+file)
-        if not (dir+file == ROOT_DIR+file):
-            copy(dir+file,ROOT_DIR+file)
+        if not (dir+file == ROOT_SRC_DIR+file):
+            copy(dir+file,ROOT_SRC_DIR+file)
 
 # Keep all files over 100MB in size
 
@@ -37,12 +38,24 @@ def rename_episodes(dir):
         rename(dir+file,dir+new_name)
 
 def move_files_to_NAS():
-    None
+    available_dirs = scan.scan_all(ROOT_TRG_DIR)
+
+    files_to_move = scan.scan_all_files(ROOT_SRC_DIR)
+    dotted_dirs = [rename_file.add_dots_remove_years(dir) for dir in available_dirs]
+    for episode in files_to_move:
+        match = [dir for dir in dotted_dirs if (episode.lower()).startswith(dir.lower())]
+        target_series_directory = rename_file.undot_file(max(match))
+        season = rename_file.split_episode_number(rename_file.match_season_episode(episode))[0]
+        season = str(season).lstrip('0')
+        print('copy ' + ROOT_SRC_DIR+episode + ' to '
+              + ROOT_TRG_DIR + target_series_directory + '\\' + 'Season ' + season + '\\' + episode)
+        #copy(ROOT_SRC_DIR+episode, ROOT_TRG_DIR + target_series_directory + '\\' + 'Season ' + season + '\\' + episode)
 
 def main():
     #flatten_directory(ROOT_DIR)
     #filter_episodes(ROOT_DIR)
-    rename_episodes(ROOT_DIR)
+    #rename_episodes(ROOT_SRC_DIR)
+    move_files_to_NAS()
 
 
 
--- rename_file.py
+++ rename_file.py
@@ -1,30 +1,36 @@
 __author__ = 'Peter'
 import re
 #
-# This script will eventually rename media files to the right
-# format for the Plex Media Server.
-# eg. Castle SxxEyy.mkv
+# This module provides functions for manipulating
+# filenames and changing them to plex default
+# eg. Castle SxxEyy
 #
 
-pattern1 = re.compile('[Ss]\\d+[Ee]\\d+')
+default_pattern = re.compile('[Ss]\\d+[Ee]\\d+')
 pattern2 = re.compile('[\.\-]\d{3,4}')
 pattern3 = re.compile('\d{1,2}[Xx]\d{1,2}')
 
 def match_season_episode(filename):
-    return pattern1.search(filename).group()
+    return default_pattern.search(filename).group()
 
 def split_episode_number(episode_number):
     episode = episode_number[-2:]
     season = episode_number[1:3]
     return (season,episode)
 
+
 def match_default_pattern(string):
-    pattern = re.compile('[Ss]\\d+[Ee]\\d+')
-    return pattern.search(string)
+    return default_pattern.search(string)
+
+def add_dots_remove_years(string):
+    return re.sub(r'.[(]\d{4}[)]','',string.replace(' ','.'))
+
+def undot_file(string):
+    return string.replace('.',' ')
 
 def plexify_name(string):
     #If the file matches the pattern we want, don't change it
-    if pattern1.search(string):
+    if default_pattern.search(string):
         return string
     elif pattern2.search(string):
         piece_to_change = list(pattern2.finditer(string))[-1]
