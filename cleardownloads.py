02c307a481beeb10e4b42733e9192f3a5fa262d0
Thustra <peter.windey@telenet.be>
2015-07-25 22:54:38 +0200

    Fixed: Deleting files after sorting
    Known bugs:
    Issue with moving series with a year affix

--- cleardownloads.py
+++ cleardownloads.py
@@ -2,7 +2,8 @@ __author__ = 'Peter'
 
 from shutil import copy,rmtree
 from os.path import getsize
-from os import remove,rename
+from os import remove,rename,chmod
+import stat
 
 import scan,rename_file
 
@@ -10,17 +11,28 @@ import scan,rename_file
 ROOT_SRC_DIR = 'E:\Downloads - finished\\'
 ROOT_TRG_DIR = 'Z:\Series\\'
 
+def remove_readonly(func, path, excinfo):
+    chmod(path, stat.S_IWRITE)
+    func(path)
+
 def flatten_directory(dir):
     root_path=dir
+    print("root path is " + root_path)
     dirlist = scan.scan_all(dir)
+    print("Dirlist")
+    print(dirlist)
     for dir in dirlist:
         flatten_directory(root_path+dir+'\\')
-        rmtree(root_path+dir)
-    filelist = scan.scan_all_files(dir)
-    for file in filelist:
-        print('Copying file from ' + dir+file + ' to ' + root_path+file)
-        if not (dir+file == ROOT_SRC_DIR+file):
-            copy(dir+file,ROOT_SRC_DIR+file)
+        print("Removing: " + root_path+dir)
+        rmtree(root_path+dir, onerror=remove_readonly)
+    if dirlist == []:
+        filelist = scan.scan_all_files(dir)
+        print("Filelist")
+        print(filelist)
+        for file in filelist:
+            print('Copying file from ' + dir+file + ' to ' + ROOT_SRC_DIR+file)
+            if not (dir+file == ROOT_SRC_DIR+file):
+                copy(dir+file,ROOT_SRC_DIR+file)
 
 # Keep all files over 100MB in size
 
@@ -29,6 +41,7 @@ def filter_episodes(dir):
     for file in filelist:
         absolute_file_path = dir+file
         if getsize(absolute_file_path) < 102400000:
+            chmod(absolute_file_path, stat.S_IWRITE)
             remove(absolute_file_path)
 
 def rename_episodes(dir):
@@ -53,9 +66,9 @@ def move_files_to_NAS():
         copy(source, target)
 
 def main():
-    #flatten_directory(ROOT_SRC_DIR)
-    #filter_episodes(ROOT_SRC_DIR)
-    #rename_episodes(ROOT_SRC_DIR)
+    flatten_directory(ROOT_SRC_DIR)
+    filter_episodes(ROOT_SRC_DIR)
+    rename_episodes(ROOT_SRC_DIR)
     move_files_to_NAS()
 
 
