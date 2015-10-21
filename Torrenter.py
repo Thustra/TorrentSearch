bb43494e82108ac06efb032e110a6a089de7cf6e
Thustra <peter.windey@telenet.be>
2015-10-21 22:01:48 +0200

    Fixed: Don't try to move files that shouldn't have been downloaded.
    Known bugs:
    Issue with moving series with a year affix

--- Torrenter.py
+++ Torrenter.py
@@ -1,1 +1,13 @@
 __author__ = 'Peter'
+
+
+import subprocess
+
+def main():
+    output = subprocess.call("python search.py")
+    print("output= " + str(output))
+    subprocess.call("C:\Program Files (x86)\qBittorrent\qbittorrent.exe")
+    subprocess.call("python cleardownloads.py")
+
+if __name__ == "__main__":
+    main()
